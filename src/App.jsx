89db01f93a1e51a79edf68927f3c154dc0a66280
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-08 17:35:07 +0530

    Created routes of the Admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from '../../Admin/src/Components/Navbar/Navbar'
 import Sidebar from '../../Admin/src/Components/Sidebar/Sidebar'
+import {Routes,Route } from  'react-router-dom'
+import Add from './Pages/Add/Add'
+import Orders from './Pages/Orders/Orders'
+import List from './Pages/List/List'
 
 const App = () => {
   return (
@@ -9,6 +13,12 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/Add' element={<Add/>}/>
+          <Route path='/List' element={<List/>}/>
+          <Route path='/Orders' element={<Orders/>}/>
+
+          </Routes> 
       </div>
     </div>
   )
