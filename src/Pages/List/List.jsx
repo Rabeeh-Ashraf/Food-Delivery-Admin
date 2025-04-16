b5476985b8259b615675a3b135f7b6786f69ad99
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-16 15:30:48 +0530

    Add functionality to view list of added food items

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -20,7 +20,29 @@ const List = () => {
       fetchlist()
     },[])
   return (
-    <div>List</div>
+    <div className='list add flex-col'>
+      <p>All Food List</p>
+      <div className='list-table'>
+        <div className='list-table-format title'>
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item,index)=>{
+            return(
+              <div key={index} className='list-table-format'>
+                <img src={`${url}/images/`+item.image} alt="" />
+                <p>{item.name}</p>
+                <p>{item.category}</p>
+                <p>${item.price}</p>
+                <p>x</p>
+              </div>
+            )
+        })}
+      </div>
+      </div>
   )
 }
 
