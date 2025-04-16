417c25fa7e0195cf931fe000a8d8545517c40749
Rabeeh-Ashraf <rabeeh.ashraf2002@gmail.com>
2025-04-16 15:09:22 +0530

    started to create view product list in admin side and connected to the Backend

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 const List = () => {
+  const url = "http://localhost:4000"
+  const [list,setList]=useState([])
+
+    const fetchlist = async()=>{
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data);
+    
+    if (response.data.success) {
+      setList(response.data.data)
+    }else{
+      toast.error("error")
+    }
+  }
+    useEffect(( )=>{
+      fetchlist()
+    },[])
   return (
     <div>List</div>
   )
